055dd42a96d67a54ca5d942575a2bf4dcc06fb3c
Debiddo <dzheng@terpmail.umd.edu>
2024-11-19 15:22:51 -0500

    Create a navigator

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,34 @@
-import { Text, StyleSheet, View, FlatList} from 'react-native'
-import React, { Component } from 'react'
-import {CATEGORIES} from '../data/dummy-data'
-import CategoryGridTile from '../components/CategoryGridTile'
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  Platform,
+  StatusBar,
+} from "react-native";
+import React, { Component } from "react";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData){
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 export default class CategoriesScreen extends Component {
   render() {
     return (
-        <FlatList data={CATEGORIES} keyExtractor={(item)=> item.id} renderItem={renderCategoryItem} numColumns={2} />
-    )
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={renderCategoryItem}
+        numColumns={2}
+      />
+    );
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+});
