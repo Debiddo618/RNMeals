aa642e515f9fc6670f949701de78f53dc617f6c8
Debiddo <dzheng@terpmail.umd.edu>
2024-11-20 18:08:49 -0500

    created the meal detail screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -10,25 +10,31 @@ import React, { Component } from "react";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
 
-export default class CategoriesScreen extends Component {
-  render() {
+function CategoriesScreen({navigation}){
+  function renderCategoryItem(itemData) {
+    function pressHandler(){
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id,
+      });
+    }
     return (
-      <FlatList
-        data={CATEGORIES}
-        keyExtractor={(item) => item.id}
-        renderItem={renderCategoryItem}
-        numColumns={2}
-      />
+      <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />
     );
   }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
 });
+
+export default CategoriesScreen;
---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,54 @@
+import { useEffect, useLayoutEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const MealsOverviewScreen = ({ route, navigation }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    // used to change the options
+    navigation.setOptions({ title: categoryTitle });
+  }, [catId, navigation]);
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default MealsOverviewScreen;
